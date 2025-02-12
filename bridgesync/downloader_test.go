944a02218e964c303bf768b44c21ef24c7327337
Stefan Negovanović <stefan@ethernal.tech>
2025-02-12 15:15:39 +0100

    test: bridgesync downloader buildAppender UT

---
+++ bridgesync/downloader_test.go
@@ -0,0 +1,136 @@
+package bridgesync
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/0xPolygon/cdk-contracts-tooling/contracts/etrog/polygonzkevmbridgev2"
+	"github.com/agglayer/aggkit/bridgesync/mocks"
+	"github.com/agglayer/aggkit/sync"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBuildAppender(t *testing.T) {
+	bridgeAddr := common.HexToAddress("0x10")
+	blockNum := uint64(1)
+	client := mocks.NewEthClienter(t)
+
+	abi, err := polygonzkevmbridgev2.Polygonzkevmbridgev2MetaData.GetAbi()
+	require.NoError(t, err)
+
+	tests := []struct {
+		name           string
+		eventSignature common.Hash
+		logBuilder     func() (types.Log, error)
+	}{
+		{
+			name:           "bridgeEventSignature appender",
+			eventSignature: bridgeEventSignature,
+			logBuilder: func() (types.Log, error) {
+				event, err := abi.EventByID(bridgeEventSignature)
+				if err != nil {
+					return types.Log{}, err
+				}
+
+				leafType := uint8(1)
+				originNetwork := uint32(10)
+				originAddress := common.HexToAddress("0x20")
+				destinationNetwork := uint32(20)
+				destinationAddress := common.HexToAddress("0x30")
+				amount := big.NewInt(100)
+				metadata := []byte{0x40}
+				depositCount := uint32(1)
+				data, err := event.Inputs.Pack(
+					leafType, originNetwork, originAddress,
+					destinationNetwork, destinationAddress,
+					amount, metadata, depositCount)
+				if err != nil {
+					return types.Log{}, err
+				}
+
+				l := types.Log{
+					Topics: []common.Hash{bridgeEventSignature},
+					Data:   data,
+				}
+				return l, nil
+			},
+		},
+		{
+			name:           "claimEventSignature appender",
+			eventSignature: claimEventSignature,
+			logBuilder: func() (types.Log, error) {
+				event, err := abi.EventByID(claimEventSignature)
+				if err != nil {
+					return types.Log{}, err
+				}
+
+				globalIndex := big.NewInt(5)
+				originNetwork := uint32(6)
+				originAddress := common.HexToAddress("0x20")
+				destinationAddress := common.HexToAddress("0x30")
+				amount := big.NewInt(10)
+				data, err := event.Inputs.Pack(
+					globalIndex, originNetwork,
+					originAddress, destinationAddress, amount)
+				if err != nil {
+					return types.Log{}, err
+				}
+
+				l := types.Log{
+					Topics: []common.Hash{claimEventSignature},
+					Data:   data,
+				}
+				return l, nil
+			},
+		},
+		{
+			name:           "tokenMappingEventSignature appender",
+			eventSignature: tokenMappingEventSignature,
+			logBuilder: func() (types.Log, error) {
+				event, err := abi.EventByID(tokenMappingEventSignature)
+				if err != nil {
+					return types.Log{}, err
+				}
+
+				originNetwork := uint32(10)
+				originTokenAddress := common.HexToAddress("0x20")
+				wrappedTokenAddress := common.HexToAddress("0x30")
+				metadata := []byte{0x40}
+				data, err := event.Inputs.Pack(
+					originNetwork, originTokenAddress,
+					wrappedTokenAddress, metadata)
+				if err != nil {
+					return types.Log{}, err
+				}
+
+				l := types.Log{
+					Topics: []common.Hash{tokenMappingEventSignature},
+					Data:   data,
+				}
+				return l, nil
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			log, err := tt.logBuilder()
+			require.NoError(t, err)
+
+			appenderMap, err := buildAppender(client, bridgeAddr, false)
+			require.NoError(t, err)
+			require.NotNil(t, appenderMap)
+
+			block := &sync.EVMBlock{EVMBlockHeader: sync.EVMBlockHeader{Num: blockNum}}
+
+			appenderFunc, exists := appenderMap[tt.eventSignature]
+			require.True(t, exists)
+
+			err = appenderFunc(block, log)
+			require.NoError(t, err)
+			require.Len(t, block.Events, 1)
+		})
+	}
+}
