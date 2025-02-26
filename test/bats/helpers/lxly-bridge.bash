055008845aa911146d05d4a67e7ccac4c9900f0d
Stefan Negovanović <stefan@ethernal.tech>
2025-02-26 10:52:21 +0100

    feat: substitute echo with logs in order not to mess up the return value

--- test/bats/helpers/lxly-bridge.bash
+++ test/bats/helpers/lxly-bridge.bash
@@ -9,25 +9,25 @@ function bridge_message() {
     local bridge_sig='bridgeMessage(uint32,address,bool,bytes)'
 
     if [[ $token_addr == "0x0000000000000000000000000000000000000000" ]]; then
-        echo "The ETH balance for sender "$sender_addr":" >&3
-        cast balance -e --rpc-url $rpc_url $sender_addr >&3
+        local eth_balance=$(cast balance -e --rpc-url "$rpc_url" "$sender_addr")
+        log "💰 $sender_addr ETH Balance: $eth_balance wei"
     else
-        echo "The "$token_addr" token balance for sender "$sender_addr":" >&3
-        echo "cast call --rpc-url $rpc_url $token_addr \"$balance_of_fn_sig\" $sender_addr" >&3
-        balance_wei=$(cast call --rpc-url "$rpc_url" "$token_addr" "$balance_of_fn_sig" "$sender_addr" | awk '{print $1}')
-        echo "$(cast --from-wei "$balance_wei")" >&3
+        local balance_wei=$(cast call --rpc-url "$rpc_url" "$token_addr" "$balance_of_fn_sig" "$sender_addr" | awk '{print $1}')
+        local token_balance=$(cast --from-wei "$balance_wei")
+        log "💎 $sender_addr Token Balance: $token_balance units [$token_addr]"
     fi
 
-    echo "Attempting to deposit $amount [wei] using bridgeMessage to $destination_addr, token $token_addr (sender=$sender_addr, network id=$destination_net, rpc url=$rpc_url)" >&3
+    log "🚀 Bridge message $amount wei → $destination_addr [network: $destination_net, token: $token_addr, rpc: $rpc_url]"
 
     if [[ $dry_run == "true" ]]; then
+        log "📝 Dry run bridge asset (showing calldata only)"
         cast calldata $bridge_sig $destination_net $destination_addr $amount $token_addr $is_forced $meta_bytes
     else
         if [[ $token_addr == "0x0000000000000000000000000000000000000000" ]]; then
-            echo "cast send --legacy --private-key $sender_private_key --value $amount --rpc-url $rpc_url $bridge_addr $bridge_sig $destination_net $destination_addr $is_forced $meta_bytes"
+            log "cast send --legacy --private-key $sender_private_key --value $amount --rpc-url $rpc_url $bridge_addr $bridge_sig $destination_net $destination_addr $is_forced $meta_bytes"
             cast send --legacy --private-key $sender_private_key --value $amount --rpc-url $rpc_url $bridge_addr $bridge_sig $destination_net $destination_addr $is_forced $meta_bytes
         else
-            echo "cast send --legacy --private-key $sender_private_key --rpc-url $rpc_url $bridge_addr $bridge_sig $destination_net $destination_addr $is_forced $meta_bytes"
+            log "cast send --legacy --private-key $sender_private_key --rpc-url $rpc_url $bridge_addr $bridge_sig $destination_net $destination_addr $is_forced $meta_bytes"
             cast send --legacy --private-key $sender_private_key --rpc-url $rpc_url $bridge_addr $bridge_sig $destination_net $destination_addr $is_forced $meta_bytes
         fi
     fi
@@ -40,14 +40,14 @@ function bridge_asset() {
 
     if [[ $token_addr == "0x0000000000000000000000000000000000000000" ]]; then
         local eth_balance=$(cast balance -e --rpc-url "$rpc_url" "$sender_addr")
-        log "💰 ETH Balance: $eth_balance wei"
+        log "💰 $sender_addr ETH Balance: $eth_balance wei"
     else
-        balance_wei=$(cast call --rpc-url "$rpc_url" "$token_addr" "$balance_of_fn_sig" "$sender_addr" | awk '{print $1}')
+        local balance_wei=$(cast call --rpc-url "$rpc_url" "$token_addr" "$balance_of_fn_sig" "$sender_addr" | awk '{print $1}')
         local token_balance=$(cast --from-wei "$balance_wei")
-        log "💎 Token Balance: $token_balance units [$token_addr]"
+        log "💎 $sender_addr Token Balance: $token_balance units [$token_addr]"
     fi
 
-    log "🚀 Bridging $amount wei → $destination_addr [network: $destination_net]"
+    log "🚀 Bridge asset $amount wei → $destination_addr [network: $destination_net]"
 
     if [[ $dry_run == "true" ]]; then
         log "📝 Dry run bridge asset (showing calldata only)"
@@ -86,28 +86,28 @@ function claim() {
 
     readonly bridge_deposit_file=$(mktemp)
     readonly claimable_deposit_file=$(mktemp)
-    echo "Getting full list of deposits" >&3
-    echo "    curl -s \"$bridge_api_url/bridges/$destination_addr?limit=100&offset=0\"" >&3
+    log "Getting full list of deposits"
+    log " curl -s \"$bridge_api_url/bridges/$destination_addr?limit=100&offset=0\""
     curl -s "$bridge_api_url/bridges/$destination_addr?limit=100&offset=0" | jq '.' | tee $bridge_deposit_file
 
-    echo "Looking for claimable deposits" >&3
+    log "Looking for claimable deposits"
     jq '[.deposits[] | select(.ready_for_claim == true and .claim_tx_hash == "" and .dest_net == '$destination_net')]' $bridge_deposit_file | tee $claimable_deposit_file
     readonly claimable_count=$(jq '. | length' $claimable_deposit_file)
-    echo "Found $claimable_count claimable deposits" >&3
+    log "Found $claimable_count claimable deposits"
 
     if [[ $claimable_count == 0 ]]; then
-        echo "We have no claimable deposits at this time" >&3
+        log "We have no claimable deposits at this time"
         exit 1
     fi
 
-    echo "We have $claimable_count claimable deposits on network $destination_net. Let's get this party started." >&3
+    log "We have $claimable_count claimable deposits on network $destination_net. Let's get this party started."
     readonly current_deposit=$(mktemp)
     readonly current_proof=$(mktemp)
     local gas_price_factor=1
     while read deposit_idx; do
-        echo "Starting claim for tx index: "$deposit_idx >&3
-        echo "Deposit info:" >&3
-        jq --arg idx $deposit_idx '.[($idx | tonumber)]' $claimable_deposit_file | tee $current_deposit >&3
+        log "Starting claim for tx index: $deposit_idx"
+        log "Deposit info:"
+        log jq --arg idx $deposit_idx '.[($idx | tonumber)]' $claimable_deposit_file | tee $current_deposit
 
         curr_deposit_cnt=$(jq -r '.deposit_cnt' $current_deposit)
         curr_network_id=$(jq -r '.network_id' $current_deposit)
@@ -130,8 +130,8 @@ function claim() {
         else
             local comp_gas_price=$(bc -l <<<"$gas_price * 1.5" | sed 's/\..*//')
             if [[ $? -ne 0 ]]; then
-                echo "Failed to calculate gas price" >&3
-                exit 1
+                log "Failed to calculate gas price"
+                return 1
             fi
 
             echo "cast send --legacy --gas-price $comp_gas_price --rpc-url $destination_rpc_url --private-key $sender_private_key $bridge_addr \"$claim_sig\" \"$in_merkle_proof\" \"$in_rollup_merkle_proof\" $in_global_index $in_main_exit_root $in_rollup_exit_root $in_orig_net $in_orig_addr $in_dest_net $in_dest_addr $in_amount $in_metadata" >&3
@@ -159,8 +159,8 @@ function claim_tx_hash() {
     local current_time=$(date +%s)
     local end_time=$((current_time + timeout))
     if [ -z $bridge_merkle_proof_url ]; then
-        echo "claim_tx_hash bad params" >&3
-        echo "claim_tx_hash: $*" >&3
+        log "❌ claim_tx_hash bad params"
+        log "❌ claim_tx_hash: $*"
         exit 1
     fi
 
@@ -168,22 +168,21 @@ function claim_tx_hash() {
         current_time=$(date +%s)
         elapsed_time=$((current_time - start_time))
         if ((current_time > end_time)); then
-            echo "[$(date '+%Y-%m-%d %H:%M:%S')] ❌ Exiting... Timeout reached waiting for tx_hash [$tx_hash] timeout: $timeout! (elapsed: $elapsed_time)"
-            echo "     $current_time > $end_time" >&3
+            log "❌ Exiting... Timeout reached waiting for tx_hash [$tx_hash] timeout: $timeout! (elapsed: $elapsed_time)"
             exit 1
         fi
         log "curl -s \"$bridge_merkle_proof_url/bridges/$destination_addr?limit=100&offset=0\""
         curl -s "$bridge_merkle_proof_url/bridges/$destination_addr?limit=100&offset=0" | jq "[.deposits[] | select(.tx_hash == \"$tx_hash\" )]" >$bridge_deposit_file
         deposit_count=$(jq '. | length' $bridge_deposit_file)
-        echo "...[$(date '+%Y-%m-%d %H:%M:%S')] deposit_count=$deposit_count bridge_deposit_file=$bridge_deposit_file" >&3
+        log "deposit_count=$deposit_count bridge_deposit_file=$bridge_deposit_file"
         if [[ $deposit_count == 0 ]]; then
-            echo "...[$(date '+%Y-%m-%d %H:%M:%S')] ❌  the tx_hash [$tx_hash] not found (elapsed: $elapsed_time / timeout:$timeout)" >&3
+            log "❌ the tx_hash [$tx_hash] not found (elapsed: $elapsed_time / timeout:$timeout)"
             sleep "$claim_frequency"
             continue
         fi
         local ready_for_claim=$(jq '.[0].ready_for_claim' $bridge_deposit_file)
         if [ $ready_for_claim != "true" ]; then
-            echo ".... [$(date '+%Y-%m-%d %H:%M:%S')] ⏳ the tx_hash $tx_hash is not ready for claim yet (elapsed: $elapsed_time / timeout:$timeout)" >&3
+            log "⏳ the tx_hash $tx_hash is not ready for claim yet (elapsed: $elapsed_time / timeout:$timeout)"
             sleep "$claim_frequency"
             continue
         else
@@ -201,6 +200,7 @@ function claim_tx_hash() {
     local curr_network_id=$(jq '.[0].network_id' $bridge_deposit_file)
     readonly current_deposit=$(mktemp)
     jq '.[(0|tonumber)]' $bridge_deposit_file | tee $current_deposit
+    log "Deposit info: $current_deposit"
     readonly current_proof=$(mktemp)
     log "requesting merkle proof for $tx_hash deposit_cnt=$curr_deposit_cnt network_id: $curr_network_id"
     request_merkle_proof "$curr_deposit_cnt" "$curr_network_id" "$bridge_merkle_proof_url" "$current_proof"
@@ -209,17 +209,17 @@ function claim_tx_hash() {
     echo "FILE current_proof=$current_proof"
 
     while true; do
-        log "requesting claim for $tx_hash"
-        run request_claim $current_deposit $current_proof $destination_rpc_url
+        log "Requesting claim for $tx_hash..."
+        request_claim $current_deposit $current_proof $destination_rpc_url
         request_result=$status
-        log "🎉 request_claim returns $request_result"
+        log "ℹ️ request_claim returns $request_result"
         if [ $request_result -eq 0 ]; then
-            log "🎉 claim successful"
+            log "🎉 Claim successful"
             break
         fi
         if [ $request_result -eq 2 ]; then
             # GlobalExitRootInvalid() let's retry
-            log "❌ claim failed, let's retry"
+            log "❌ Claim failed, let's retry"
             current_time=$(date +%s)
             elapsed_time=$((current_time - start_time))
             if ((current_time > end_time)); then
@@ -230,13 +230,13 @@ function claim_tx_hash() {
             continue
         fi
         if [ $request_result -ne 0 ]; then
-            log "✅  claim successful tx_hash [$tx_hash]"
+            log "✅ Claim successful tx_hash [$tx_hash]"
             exit 1
         fi
     done
 
     local global_index=$(jq '.global_index' $current_deposit | sed -e 's/\x1b\[[0-9;]*m//g' | tr -d '"')
-    log "$global_index deposit claimed"
+    log "Deposit claimed ($global_index)"
     echo $global_index
 
     # clean up temp files
@@ -289,11 +289,11 @@ function request_claim() {
     else
         local comp_gas_price=$(bc -l <<<"$gas_price * 1.5" | sed 's/\..*//')
         if [[ $? -ne 0 ]]; then
-            echo "Failed to calculate gas price" >&3
-            exit 1
+            log "❌ Failed to calculate gas price" >&3
+            return 1
         fi
-        echo "... Claiming deposit: global_index: $in_global_index orig_net: $in_orig_net dest_net: $in_dest_net  amount:$in_amount" >&3
-        echo "claim: mainnetExitRoot=$in_main_exit_root  rollupExitRoot=$in_rollup_exit_root"
+        log "Claiming deposit: global_index: $in_global_index orig_net: $in_orig_net dest_net: $in_dest_net amount:$in_amount"
+        echo "claim: mainnetExitRoot=$in_main_exit_root rollupExitRoot=$in_rollup_exit_root"
         echo "cast send --legacy --gas-price $comp_gas_price --rpc-url $destination_rpc_url --private-key $sender_private_key $bridge_addr \"$claim_sig\" \"$in_merkle_proof\" \"$in_rollup_merkle_proof\" $in_global_index $in_main_exit_root $in_rollup_exit_root $in_orig_net $in_orig_addr $in_dest_net $in_dest_addr $in_amount $in_metadata"
         local tmp_response=$(mktemp)
         cast send --legacy --gas-price $comp_gas_price \
@@ -306,20 +306,20 @@ function request_claim() {
 function check_claim_revert_code() {
     local file_curl_reponse="$1"
     # 0x646cf558 -> AlreadyClaimed()
-    echo "check revert "
+    log "check revert"
     cat $file_curl_reponse
     cat $file_curl_reponse | grep "0x646cf558" >/dev/null
     if [ $? -eq 0 ]; then
-        echo "....[$(date '+%Y-%m-%d %H:%M:%S')] 🎉  deposit is already claimed (revert code 0x646cf558)" >&3
+        log "🎉 Deposit is already claimed (revert code 0x646cf558)"
         return 0
     fi
     cat $file_curl_reponse | grep "0x002f6fad" >/dev/null
     if [ $? -eq 0 ]; then
-        echo "....[$(date '+%Y-%m-%d %H:%M:%S')] 🎉  GlobalExitRootInvalid()(revert code 0x002f6fad)" >&3
+        log "🎉 GlobalExitRootInvalid()(revert code 0x002f6fad)"
         return 2
     fi
-    echo "....[$(date '+%Y-%m-%d %H:%M:%S')] ❌  claim failed" >&3
-    cat $file_curl_reponse >&3
+    log "❌ Claim failed"
+    log $file_curl_reponse
     return 1
 }
 
@@ -334,8 +334,8 @@ function wait_for_claim() {
     while true; do
         local current_time=$(date +%s)
         if ((current_time > end_time)); then
-            echo "[$(date '+%Y-%m-%d %H:%M:%S')] ❌ Exiting... Timeout reached!"
-            exit 1
+            log "❌ Exiting... Timeout reached!"
+            return 1
         fi
 
         run claim $destination_rpc_url $bridge_type
