1a41c6348e8b07e0e05a7df0107ad9bb93f3445e
Goran Rojovic <goran.rojovic@ethernal.tech>
2025-04-17 14:45:58 +0200

    fix: comments

--- aggsender/db/migrations/0002.sql
+++ aggsender/db/migrations/0002.sql
@@ -4,7 +4,7 @@ ALTER TABLE certificate_info DROP COLUMN finalized_l1_info_tree_root;
 ALTER TABLE certificate_info DROP COLUMN l1_info_tree_leaf_count;
 ALTER TABLE certificate_info_history DROP COLUMN aggchain_proof;
 ALTER TABLE certificate_info_history DROP COLUMN finalized_l1_info_tree_root;
-ALTER TABLE certificate_info DROP COLUMN l1_info_tree_leaf_count;
+ALTER TABLE certificate_info_history DROP COLUMN l1_info_tree_leaf_count;
 
 -- +migrate Up
 ALTER TABLE certificate_info ADD COLUMN aggchain_proof BLOB;
