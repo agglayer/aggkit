16a12e89a14c27e8f0922a06b26b8f658520faf2
Stefan Negovanović <stefan@ethernal.tech>
2025-04-23 16:08:19 +0200

    minor fixes & cleanups

--- test/bats/helpers/common-setup.bash
+++ test/bats/helpers/common-setup.bash
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 _common_setup() {
-    bats_load_library 'bats-support' || return 1
-    bats_load_library 'bats-assert' || return 1
+    bats_load_library 'bats-support'
+    if [ $? -ne 0 ]; then return 1; fi
+
+    bats_load_library 'bats-assert'
+    if [ $? -ne 0 ]; then return 1; fi
 
     # Resolve project root
-    PROJECT_ROOT="$(cd "$(dirname "$BATS_TEST_FILENAME")/.." >/dev/null 2>&1 && pwd)" || {
+    PROJECT_ROOT="$(cd "$(dirname "$BATS_TEST_FILENAME")/.." >/dev/null 2>&1 && pwd)"
+    if [ $? -ne 0 ]; then
         echo "❌ Failed to determine PROJECT_ROOT" >&2
         return 1
-    }
+    fi
 
     PATH="$PROJECT_ROOT/src:$PATH"
 
@@ -25,10 +29,13 @@ _common_setup() {
     if [[ -z "${L2_ETH_RPC_URL}" ]]; then
         readonly l2_rpc_node="${L2_RPC_NODE:-cdk-erigon-rpc-001}"
         echo "ℹ️ L2_ETH_RPC_URL not provided, resolving from Kurtosis (L2_RPC_NODE: $l2_rpc_node)" >&3
-        readonly l2_rpc_url="$(kurtosis port print "$enclave" "$l2_rpc_node" rpc)" || {
+
+        tmp_l2_rpc_url=$(kurtosis port print "$enclave" "$l2_rpc_node" rpc)
+        if [ $? -ne 0 ]; then
             echo "❌ Failed to resolve L2 RPC URL from Kurtosis" >&2
             return 1
-        }
+        fi
+        readonly l2_rpc_url="$tmp_l2_rpc_url"
     else
         readonly l2_rpc_url="$L2_ETH_RPC_URL"
     fi
@@ -38,24 +45,28 @@ _common_setup() {
     if [[ -z "${L1_BRIDGE_ADDRESS}" || -z "${L2_BRIDGE_ADDRESS}" ]]; then
         local combined_json_file="/opt/zkevm/combined.json"
         echo "ℹ️ Some bridge addresses are missing, fetching from CDK: $combined_json_file" >&3
-        combined_json_output="$($contracts_service_wrapper "cat $combined_json_file" | tail -n +2)" || {
+
+        combined_json_output="$($contracts_service_wrapper "cat $combined_json_file" | tail -n +2)"
+        if [ $? -ne 0 ]; then
             echo "❌ Failed to read "$combined_json_file" from Kurtosis CDK" >&2
             return 1
-        }
+        fi
     fi
 
     if [[ -z "${L1_BRIDGE_ADDRESS}" ]]; then
-        L1_BRIDGE_ADDRESS="$(echo "$combined_json_output" | jq -r .polygonZkEVMBridgeAddress)" || {
+        L1_BRIDGE_ADDRESS="$(echo "$combined_json_output" | jq -r .polygonZkEVMBridgeAddress)"
+        if [ $? -ne 0 ]; then
             echo "❌ Failed to extract L1_BRIDGE_ADDRESS from "$combined_json_file"" >&2
             return 1
-        }
+        fi
     fi
 
     if [[ -z "${L2_BRIDGE_ADDRESS}" ]]; then
-        L2_BRIDGE_ADDRESS="$(echo "$combined_json_output" | jq -r .polygonZkEVML2BridgeAddress)" || {
+        L2_BRIDGE_ADDRESS="$(echo "$combined_json_output" | jq -r .polygonZkEVML2BridgeAddress)"
+        if [ $? -ne 0 ]; then
             echo "❌ Failed to extract L2_BRIDGE_ADDRESS from "$combined_json_file"" >&2
             return 1
-        }
+        fi
     fi
 
     echo "✅ L1 bridge address=$L1_BRIDGE_ADDRESS; L2 bridge address=$L2_BRIDGE_ADDRESS" >&3
