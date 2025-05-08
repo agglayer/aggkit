cca5b03bf1eab50694579bd62f205166af21b076
Stefan Negovanović <stefan@ethernal.tech>
2025-05-08 12:58:42 +0200

    test: GetLegacyTokenMigrations UT

--- bridgeservice/utils.go
+++ bridgeservice/utils.go
@@ -34,33 +34,6 @@ func validatePaginationParams(pageNumber, pageSize uint32) error {
 	return nil
 }
 
-// TODO: @Stefan-Ethernal REMOVE
-func validatePaginationParamsOld(pageNumber, pageSize *uint32) (uint32, uint32, error) {
-	if pageNumber == nil {
-		pageNumber = new(uint32)
-		*pageNumber = DefaultPage
-	}
-
-	if pageSize == nil {
-		pageSize = new(uint32)
-		*pageSize = DefaultPageSize
-	}
-
-	if *pageNumber == 0 {
-		return 0, 0, bridgesync.ErrInvalidPageNumber
-	}
-
-	if *pageSize == 0 {
-		return 0, 0, bridgesync.ErrInvalidPageSize
-	}
-
-	if *pageSize > MaxPageSize {
-		return 0, 0, fmt.Errorf("page size must be less than or equal to %d", MaxPageSize)
-	}
-
-	return *pageNumber, *pageSize, nil
-}
-
 type UintParam interface {
 	~uint32 | ~uint64
 }
