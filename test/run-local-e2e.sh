d38cea46c671a0eef1603117f71d4b9cf1001f43
Stefan Negovanović <stefan@ethernal.tech>
2025-05-16 08:18:25 +0200

    run sovereign chain test only for op stack

--- test/run-local-e2e.sh
+++ test/run-local-e2e.sh
@@ -97,7 +97,7 @@ if [ "$TEST_TYPE" == "single-l2-network-fork12-op-succinct" ]; then
     bats ./tests/aggkit/bridge-e2e.bats ./tests/aggkit/bridge-sovereign-chain-e2e.bats
 elif [ "$TEST_TYPE" == "single-l2-network-fork12-pessimistic" ]; then
     export DISABLE_L2_FUND="true"
-    bats ./tests/aggkit/bridge-e2e-custom-gas.bats ./tests/aggkit/bridge-e2e.bats
+    bats ./tests/aggkit/bridge-e2e.bats ./tests/aggkit/bridge-e2e-custom-gas.bats 
 elif [ "$TEST_TYPE" == "multi-l2-networks" ]; then
     export DISABLE_L2_FUND="true"
     bats ./tests/aggkit/bridge-l2_to_l2-e2e.bats
