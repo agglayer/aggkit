7564c9e0539d9762490079a6e27503f1f2be91ec
Stefan Negovanović <stefan@ethernal.tech>
2025-06-02 15:36:11 +0200

    remove unnecessary logic from etherman

--- common/config.go
+++ common/config.go
@@ -10,13 +10,16 @@ import (
 
 // Config holds the configuration for the Aggkit.
 type Config struct {
+	// TODO: @Stefan-Ethernal DEPRECATE
 	// IsValidiumMode has the value true if the sequence sender is running in validium mode.
 	IsValidiumMode bool `mapstructure:"IsValidiumMode"`
 	// NetworkID is the networkID of the Aggkit being run
 	NetworkID uint32 `mapstructure:"NetworkID"`
+	// TODO: @Stefan-Ethernal DEPRECATE
 	// Contract Versions: elderberry, banana
-	ContractVersions string            `mapstructure:"ContractVersions"`
-	Translator       translator.Config `mapstructure:"Translator"`
+	ContractVersions string `mapstructure:"ContractVersions"`
+	// TODO: @Stefan-Ethernal DEPRECATE
+	Translator translator.Config `mapstructure:"Translator"`
 	// L2URL is the URL of the L2 node
 	L2RPC ethermanconfig.RPCClientConfig `mapstructure:"L2RPC"`
 }
