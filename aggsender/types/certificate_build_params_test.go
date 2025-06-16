4b83500a9ad7becb4801fbe0db9e7afb1e8f55e4
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2025-06-16 11:48:39 +0200

    fix: coverage

--- aggsender/types/certificate_build_params_test.go
+++ aggsender/types/certificate_build_params_test.go
@@ -6,11 +6,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestXxx(t *testing.T) {
+func TestInvalidRangeToBlock(t *testing.T) {
 	params := &CertificateBuildParams{
 		FromBlock: 100,
 		ToBlock:   200,
 	}
 	_, err := params.Range(100, 0)
+	require.Error(t, err, "should return an error for invalid range")
+}
+
+func TestInvalidRangeOutsideOriginalRange(t *testing.T) {
+	params := &CertificateBuildParams{
+		FromBlock: 100,
+		ToBlock:   200,
+	}
+	_, err := params.Range(99, 110)
 	require.Error(t, err, "should return an error for invalid range")
 }
