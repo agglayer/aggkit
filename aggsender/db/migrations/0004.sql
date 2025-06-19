9e3a0c47a1bf68f7ccdbccc48c0278bd1fc3d76a
Joan Esteban <129153821+joanestebanr@users.noreply.github.com>
2025-06-19 14:24:04 +0200

    fix: migration 0.2 to 0.3 issue  (#661)
    ## Description
    
    The migration from aggkit `v0.2.0` to `v0.3.0` fails if there are any certificate in database.  Procude next error: `sql: Scan error on column index 4, name "previous_local_exit_root": converting NULL to string is unsupported`
    
    - Add support to Meddler to `*common.Hash`
    - Remove no settled certificates with  `l1_info_tree_leaf_count` NULL (it could be used on code with a bad  `l1_info_tree_leaf_count` value)
    - Set `l1_info_tree_leaf_count` NULL to 0 on database

---
+++ aggsender/db/migrations/0004.sql
@@ -0,0 +1,14 @@
+--- Fix migration from 0001 to 0002 that field l1_info_tree_leaf_count can't be read
+-- +migrate Down
+
+
+-- +migrate Up
+
+-- The field l1_info_tree_leaf_count is unknown for previous data stored in DB
+-- the only place where is used is to retry a FEP. So we delete all non-finalized certificates
+-- so it can't produce the error keeping as much data as possible.
+DELETE FROM certificate_info WHERE  status != 4 AND l1_info_tree_leaf_count IS NULL; -- Remove non-finalized certificates with NULL l1_info_tree_leaf_count
+DELETE FROM certificate_info_history WHERE status != 4 AND l1_info_tree_leaf_count IS NULL; -- Remove non-finalized certificates with NULL l1_info_tree_leaf_count
+--- The rest of l1info_tree_leaf_count is not used in the code, so we can safely set to 0
+UPDATE certificate_info SET l1_info_tree_leaf_count = 0 WHERE l1_info_tree_leaf_count IS NULL;
+UPDATE certificate_info_history SET l1_info_tree_leaf_count = 0 WHERE l1_info_tree_leaf_count IS NULL;
