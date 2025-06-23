a0c4d22658d1d6b6fec89d9894e108c163bc0ec2
Goran Rojovic <goran.rojovic@ethernal.tech>
2025-06-23 10:45:58 +0200

    fix: comment

--- aggsender/db/migrations/0004.sql
+++ aggsender/db/migrations/0004.sql
@@ -7,7 +7,7 @@
 -- The field l1_info_tree_leaf_count is unknown for previous data stored in DB
 -- the only place where is used is to retry a FEP. So we delete all non-finalized certificates
 -- so it can't produce the error keeping as much data as possible.
-DELETE FROM certificate_info WHERE  status != 4 AND l1_info_tree_leaf_count IS NULL; -- Remove non-finalized certificates with NULL l1_info_tree_leaf_count
+DELETE FROM certificate_info WHERE status != 4 AND l1_info_tree_leaf_count IS NULL; -- Remove non-finalized certificates with NULL l1_info_tree_leaf_count
 DELETE FROM certificate_info_history WHERE status != 4 AND l1_info_tree_leaf_count IS NULL; -- Remove non-finalized certificates with NULL l1_info_tree_leaf_count
 --- The rest of l1info_tree_leaf_count is not used in the code, so we can safely set to 0
 UPDATE certificate_info SET l1_info_tree_leaf_count = 0 WHERE l1_info_tree_leaf_count IS NULL;
