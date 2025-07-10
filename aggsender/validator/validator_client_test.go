b711d9130755d0ed8a9e537ec7965100f7bd348c
Goran Rojovic <goran.rojovic@ethernal.tech>
2025-07-10 09:16:22 +0200

    feat: validator client

---
+++ aggsender/validator/validator_client_test.go
@@ -0,0 +1,173 @@
+package validator
+
+import (
+	"context"
+	"errors"
+	"math/big"
+	"testing"
+
+	agglayergrpc "github.com/agglayer/aggkit/agglayer/grpc"
+	agglayertypes "github.com/agglayer/aggkit/agglayer/types"
+	"github.com/agglayer/aggkit/aggsender/mocks"
+	v1 "github.com/agglayer/aggkit/aggsender/validator/proto/v1"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestClient_ValidatCertificate(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	prevCertHash := common.HexToHash("0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef")
+	certificate := &agglayertypes.Certificate{
+		NetworkID:         12,
+		Height:            11,
+		PrevLocalExitRoot: common.HexToHash("0x1"),
+		NewLocalExitRoot:  common.HexToHash("0x2"),
+		BridgeExits: []*agglayertypes.BridgeExit{
+			{
+				LeafType: agglayertypes.LeafTypeAsset,
+				TokenInfo: &agglayertypes.TokenInfo{
+					OriginTokenAddress: common.HexToAddress("0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890"),
+					OriginNetwork:      12,
+				},
+				DestinationNetwork: 1,
+				DestinationAddress: common.HexToAddress("0xabcdefabcdefabcdefabcdefabcdefabcdef"),
+				Amount:             big.NewInt(1000),
+				Metadata:           []byte("bridge-exit-metadata"),
+			},
+		},
+		ImportedBridgeExits: []*agglayertypes.ImportedBridgeExit{
+			{
+				GlobalIndex: &agglayertypes.GlobalIndex{
+					MainnetFlag: false,
+					RollupIndex: 111,
+					LeafIndex:   11,
+				},
+				BridgeExit: &agglayertypes.BridgeExit{
+					LeafType: agglayertypes.LeafTypeAsset,
+					TokenInfo: &agglayertypes.TokenInfo{
+						OriginNetwork:      1,
+						OriginTokenAddress: common.HexToAddress("0x1211"),
+					},
+					DestinationNetwork: 12,
+					DestinationAddress: common.HexToAddress("0x2232"),
+					Amount:             big.NewInt(100),
+					Metadata:           []byte("metadata-1"),
+				},
+				ClaimData: &agglayertypes.ClaimFromMainnnet{
+					ProofLeafMER: &agglayertypes.MerkleProof{
+						Root:  common.HexToHash("0x1"),
+						Proof: [32]common.Hash{},
+					},
+					ProofGERToL1Root: &agglayertypes.MerkleProof{
+						Root:  common.HexToHash("0x3"),
+						Proof: [32]common.Hash{},
+					},
+					L1Leaf: &agglayertypes.L1InfoTreeLeaf{
+						L1InfoTreeIndex: 1,
+						RollupExitRoot:  common.HexToHash("0x4"),
+						MainnetExitRoot: common.HexToHash("0x5"),
+						Inner: &agglayertypes.L1InfoTreeLeafInner{
+							GlobalExitRoot: common.HexToHash("0x6"),
+							BlockHash:      common.HexToHash("0x7"),
+							Timestamp:      12321,
+						},
+					},
+				},
+			},
+		},
+		Metadata:            common.HexToHash("0x3"),
+		CustomChainData:     []byte("custom-data"),
+		L1InfoTreeLeafCount: 100,
+		AggchainData: &agglayertypes.AggchainDataSignature{
+			Signature: []byte("valid-signature"),
+		},
+	}
+
+	testCases := []struct {
+		name                  string
+		previousCertificateID *common.Hash
+		certificate           *agglayertypes.Certificate
+		mockFn                func(*mocks.AggsenderValidatorClient)
+		expectedError         string
+	}{
+		{
+			name:                  "Invalid certificate - nil certificate",
+			previousCertificateID: nil,
+			certificate:           nil,
+			mockFn:                nil,
+			expectedError:         "nil certificate provided for conversion to proto",
+		},
+		{
+			name:                  "client returns an error",
+			previousCertificateID: &prevCertHash,
+			certificate:           certificate,
+			mockFn: func(mockClient *mocks.AggsenderValidatorClient) {
+				protoCert, err := agglayergrpc.ConvertCertToProtoCertificate(certificate)
+				require.NoError(t, err)
+
+				mockClient.EXPECT().ValidateCertificate(ctx, &v1.ValidateCertificateRequest{
+					PreviousCertificateId: certIDToProtoNullable(&prevCertHash),
+					Certificate:           protoCert,
+				}).Return(nil, errors.New("some error"))
+			},
+			expectedError: "some error",
+		},
+		{
+			name:                  "Valid certificate - no previous certificate",
+			previousCertificateID: nil,
+			certificate:           certificate,
+			mockFn: func(mockClient *mocks.AggsenderValidatorClient) {
+				protoCert, err := agglayergrpc.ConvertCertToProtoCertificate(certificate)
+				require.NoError(t, err)
+
+				mockClient.EXPECT().ValidateCertificate(ctx, &v1.ValidateCertificateRequest{
+					PreviousCertificateId: nil,
+					Certificate:           protoCert,
+				}).Return(&emptypb.Empty{}, nil)
+			},
+			expectedError: "",
+		},
+		{
+			name:                  "Valid certificate - with previous certificate",
+			previousCertificateID: &prevCertHash,
+			certificate:           certificate,
+			mockFn: func(mockClient *mocks.AggsenderValidatorClient) {
+				protoCert, err := agglayergrpc.ConvertCertToProtoCertificate(certificate)
+				require.NoError(t, err)
+
+				mockClient.EXPECT().ValidateCertificate(ctx, &v1.ValidateCertificateRequest{
+					PreviousCertificateId: certIDToProtoNullable(&prevCertHash),
+					Certificate:           protoCert,
+				}).Return(&emptypb.Empty{}, nil)
+			},
+			expectedError: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockClient := mocks.NewAggsenderValidatorClient(t)
+			if tc.mockFn != nil {
+				tc.mockFn(mockClient)
+			}
+
+			validatorClient := &ValidatorClient{
+				client: mockClient,
+			}
+
+			err := validatorClient.ValidateCertificate(ctx, tc.previousCertificateID, tc.certificate)
+			if tc.expectedError != "" {
+				require.ErrorContains(t, err, tc.expectedError)
+			} else {
+				require.NoError(t, err)
+			}
+
+			mockClient.AssertExpectations(t)
+		})
+	}
+}
