17656507b79516c26f0854648e01461e2c1767f5
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2025-07-15 09:58:43 +0200

    fix: PR comment

--- aggsender/validator/cert_hash.go
+++ aggsender/validator/cert_hash.go
@@ -13,6 +13,8 @@ const (
 	u64Size = 8
 )
 
+// HashCertificateToSign is the hash of the certificate that the validator will sign
+// before returning result to the aggsender
 func HashCertificateToSign(cert *agglayertypes.Certificate) common.Hash {
 	globalIndexHashes := make([][]byte, len(cert.ImportedBridgeExits))
 	for i, importedBridgeExit := range cert.ImportedBridgeExits {
