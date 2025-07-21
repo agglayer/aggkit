29e4500a463a3cce57088533ad5347577f4d85da
Arpit Temani <temaniarpit27@gmail.com>
2025-07-21 20:28:28 +0400

    fix linters

--- aggoracle/chaingersender/evm_test.go
+++ aggoracle/chaingersender/evm_test.go
@@ -664,7 +664,7 @@ func TestEVMChainGERSender_IsGERProposed(t *testing.T) {
 		},
 	}
 
-		for _, tt := range tests {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mockAggOracleCommittee := mocks.NewAggOracleCommitteeContract(t)
 			mockEthTxManager := mocks.NewEthTxManager(t)
