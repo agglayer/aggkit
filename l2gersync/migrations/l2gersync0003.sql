ef2350aa65c2df233d72ba80d58abed13dde20f6
Stefan Negovanović <stefan@ethernal.tech>
2025-07-23 12:27:44 +0200

    fix: add 0 as default value for block_pos

--- l2gersync/migrations/l2gersync0003.sql
+++ l2gersync/migrations/l2gersync0003.sql
@@ -2,4 +2,4 @@
 ALTER TABLE imported_global_exit_root DROP COLUMN block_pos;
 
 -- +migrate Up
-ALTER TABLE imported_global_exit_root ADD block_pos INTEGER NULL;
+ALTER TABLE imported_global_exit_root ADD COLUMN block_pos INTEGER NOT NULL DEFAULT 0;
