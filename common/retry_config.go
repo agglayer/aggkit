abeb790fbd8c9cc9dc4c846f505e9b739b79ad6b
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2025-07-24 11:29:15 +0200

    feat: add a cache to avoid build each time obj

--- common/retry_config.go
+++ common/retry_config.go
@@ -15,7 +15,8 @@ const (
 )
 
 var (
-	ErrInvalidRetryConfigMode = fmt.Errorf("invalid retry config mode")
+	ErrInvalidRetryConfigMode                       = fmt.Errorf("invalid retry config mode")
+	_                         RetryPolicyConfigurer = (*RetryPolicyGenericConfig)(nil)
 )
 
 // RetryPolicyGenericConfig defines the configuration for retry policies in the system.
@@ -28,6 +29,8 @@ type RetryPolicyGenericConfig struct {
 	InitialBackoff    types.Duration
 	MaxBackoff        types.Duration
 	BackoffMultiplier float64
+	// internal field to cache the underlaying config
+	cache RetryPolicyConfigurer `mapstructure:"-"`
 }
 
 // RetryPolicyConfigurer is an interface that defines methods for configuring retry policies.
@@ -51,6 +54,20 @@ func (r *RetryPolicyGenericConfig) Validate() error {
 	return cfg.Validate()
 }
 
+func (r *RetryPolicyGenericConfig) NewRetryHandler() *RetryHandler {
+	cfg, err := r.Factory()
+	if err != nil {
+		return NewRetryHandler(nil, 0) // return a no-op handler
+	}
+	return cfg.NewRetryHandler()
+}
+func (r *RetryPolicyGenericConfig) Brief() string {
+	cfg, err := r.Factory()
+	if err != nil {
+		return fmt.Sprintf("RetryPolicyConfig{Error: %s}", err)
+	}
+	return fmt.Sprintf("%s/%s", r.Mode, cfg.Brief())
+}
 func (r *RetryPolicyGenericConfig) String() string {
 	cfg, err := r.Factory()
 	if err != nil {
@@ -62,24 +79,37 @@ func (r *RetryPolicyGenericConfig) String() string {
 	return fmt.Sprintf("RetryPolicyConfig{Mode: %s, Config: %s}", r.Mode, cfg.String())
 }
 
+// CleanCache clears the internal cache of the RetryPolicyGenericConfig.
+// useful is you modify any params and want to rebuild the underlaying object
+func (r *RetryPolicyGenericConfig) CleanCache() {
+	// Clean the cache to force a new Factory call next time
+	r.cache = nil
+}
+
 func (r *RetryPolicyGenericConfig) Factory() (RetryPolicyConfigurer, error) {
+	if r.cache != nil {
+		return r.cache, nil
+	}
+	var cache RetryPolicyConfigurer
 	switch r.Mode {
 	case RetryConfigModeDelays:
-		return &RetryDelaysConfig{
+		cache = &RetryDelaysConfig{
 			Delays:     r.Delays,
 			MaxRetries: r.MaxRetries,
-		}, nil
+		}
 	case RetryConfigModeBackoff:
-		return &RetryBackoffConfig{
+		cache = &RetryBackoffConfig{
 			InitialBackoff:    r.InitialBackoff,
 			MaxBackoff:        r.MaxBackoff,
 			BackoffMultiplier: r.BackoffMultiplier,
 			MaxRetries:        r.MaxRetries,
-		}, nil
+		}
 	case RetryConfigModeNoRetries:
-		return &RetryDelaysConfig{MaxRetries: 0}, nil
+		cache = &RetryDelaysConfig{MaxRetries: 0}
 
 	default:
 		return nil, fmt.Errorf("%w: bad mode %s", ErrInvalidRetryConfigMode, r.Mode)
 	}
+	r.cache = cache
+	return cache, nil
 }
