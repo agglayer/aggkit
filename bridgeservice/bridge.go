ddfd640ca7023bae6683d8f29666af5e25c29cc6
Arpit Temani <temaniarpit27@gmail.com>
2025-08-07 14:02:23 +0400

    fix: bug when bridge l1 syncer is stopped and l1 info tree syncer continues (#833)
    ## 🔄 Changes Summary
    - fallback mechanism where if we dont have latest l1infotree index in l1
    or l2 dbs
    
    ## 🐞 Issues
    - Closes #827

--- bridgeservice/bridge.go
+++ bridgeservice/bridge.go
@@ -968,7 +968,15 @@ func (b *BridgeService) getFirstL1InfoTreeIndexForL1Bridge(ctx context.Context, 
 
 	root, err := b.bridgeL1.GetRootByLER(ctx, lastInfo.MainnetExitRoot)
 	if err != nil {
-		return 0, err
+		b.logger.Infof(
+			"failed to get root by LER for L1: %v, lastInfo MainnetExitRoot: %v, using fallback mechanism",
+			err,
+			lastInfo.MainnetExitRoot,
+		)
+		root, err = b.bridgeL1.GetLastRoot(ctx)
+		if err != nil {
+			return 0, fmt.Errorf("failed to get last root for L1: %w", err)
+		}
 	}
 	if root.Index < depositCount {
 		return 0, ErrNotOnL1Info
@@ -1021,7 +1029,15 @@ func (b *BridgeService) getFirstL1InfoTreeIndexForL2Bridge(ctx context.Context, 
 
 	root, err := b.bridgeL2.GetRootByLER(ctx, lastVerified.ExitRoot)
 	if err != nil {
-		return 0, err
+		b.logger.Infof(
+			"failed to get root by LER for L2: %v, lastVerified ExitRoot: %v, using fallback mechanism",
+			err,
+			lastVerified.ExitRoot,
+		)
+		root, err = b.bridgeL2.GetLastRoot(ctx)
+		if err != nil {
+			return 0, fmt.Errorf("failed to get last root for L2: %w", err)
+		}
 	}
 	if root.Index < depositCount {
 		return 0, ErrNotOnL1Info
