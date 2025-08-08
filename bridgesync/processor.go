0f00be5c30e285f6e9bd1bee68979e55c7e7480c
Stefan Negovanović <stefan@ethernal.tech>
2025-08-08 06:53:29 +0200

    fix: suppress duplicated warnings

--- bridgesync/processor.go
+++ bridgesync/processor.go
@@ -374,6 +374,7 @@ func newProcessor(dbPath string, name string, logger *log.Logger) (*processor, e
 	}, nil
 }
 
+//nolint:dupl
 func (p *processor) GetBridges(
 	ctx context.Context, fromBlock, toBlock uint64,
 ) ([]Bridge, error) {
@@ -407,6 +408,7 @@ func (p *processor) GetBridges(
 	return bridges, nil
 }
 
+//nolint:dupl
 func (p *processor) GetClaims(ctx context.Context, fromBlock, toBlock uint64) ([]Claim, error) {
 	rows, err := p.queryBlockRange(p.db, fromBlock, toBlock, claimTableName)
 	if err != nil {
