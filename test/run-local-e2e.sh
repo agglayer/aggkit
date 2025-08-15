5f24c22c80938e24c00cb95c4e890f41da7ff482
Arpit Temani <temaniarpit27@gmail.com>
2025-08-15 15:46:03 +0400

    update ref

--- test/run-local-e2e.sh
+++ test/run-local-e2e.sh
@@ -158,10 +158,10 @@ if [ "$E2E_REPO_PATH" != "-" ]; then
     log_info "Running BATS E2E tests..."
     case "$TEST_TYPE" in
     single-l2-network-fork12-op-succinct)
-        bats ./tests/aggkit/bridge-e2e.bats \
+        bats ./tests/op/optimistic-mode.bats \
+             ./tests/aggkit/bridge-e2e.bats \
              ./tests/aggkit/e2e-pp.bats \
              ./tests/aggkit/bridge-sovereign-chain-e2e.bats \
-             ./tests/op/optimistic-mode.bats \
              ./tests/aggkit/bridge-e2e-nightly.bats \
              ./tests/aggkit/internal-claims.bats \
              ./tests/aggkit/claim-reetrancy.bats
