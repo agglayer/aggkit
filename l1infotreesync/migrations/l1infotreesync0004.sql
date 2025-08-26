a688b2622082f226a9a60ace0ea7192a7c0d0b7e
Arpit Temani <temaniarpit27@gmail.com>
2025-08-26 13:04:22 +0400

    fix indexes

--- l1infotreesync/migrations/l1infotreesync0004.sql
+++ l1infotreesync/migrations/l1infotreesync0004.sql
@@ -1,11 +1,11 @@
 -- +migrate Down
 DROP INDEX IF EXISTS idx_l1info_leaf_position;
-DROP INDEX IF EXISTS idx_l1info_leaf_block_num_composite;
+DROP INDEX IF EXISTS idx_l1info_leaf_block_num_block_pos;
 DROP INDEX IF EXISTS idx_verify_batches_rollup_id;
 DROP INDEX IF EXISTS idx_verify_batches_block_num_block_pos;
 
 -- +migrate Up
 CREATE INDEX idx_l1info_leaf_position ON l1info_leaf(position);
-CREATE INDEX idx_l1info_leaf_block_num_composite ON l1info_leaf(block_num, block_pos);
+CREATE INDEX idx_l1info_leaf_block_num_block_pos ON l1info_leaf(block_num, block_pos);
 CREATE INDEX idx_verify_batches_rollup_id ON verify_batches(rollup_id);
 CREATE INDEX idx_verify_batches_block_num_block_pos ON verify_batches(block_num, block_pos);
