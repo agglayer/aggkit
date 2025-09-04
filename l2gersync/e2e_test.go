0732d9fdea0a3756c01058a549906c2d10537e17
Rachit Sonthalia <rachitsonthalia02@gmail.com>
2025-09-04 13:38:40 +0530

    wip

--- l2gersync/e2e_test.go
+++ l2gersync/e2e_test.go
@@ -23,7 +23,7 @@ const (
 	waitForNewBlocksPeriod     = 30 * time.Millisecond
 	syncBlockChunkSize         = 10
 	testIterations             = 5
-	syncDelay                  = 1 * time.Second
+	syncDelay                  = 500 * time.Millisecond
 )
 
 func TestL2GERSyncE2E(t *testing.T) {
@@ -121,8 +121,6 @@ func TestL2GERSync_GERRemoval(t *testing.T) {
 
 func TestL2GERSync_IndexLegacyGERManagerSC(t *testing.T) {
 	t.Parallel()
-	t.Skip("Skipping E2E test, this test is broken and needs a PR to be fixed. The lastProcessedBlock doesn't take in account empty blocks")
-
 	ctx := context.Background()
 	l1Setup, l2Setup := helpers.NewSimulatedEVMEnvironment(t, helpers.DefaultEnvironmentConfig(helpers.LegacyL2GERContract))
 
@@ -155,9 +153,9 @@ func TestL2GERSync_IndexLegacyGERManagerSC(t *testing.T) {
 		time.Sleep(syncDelay)
 	}
 
-	l1Setup.SimBackend.Commit()
-	l2Setup.SimBackend.Commit()
-	time.Sleep(1 * time.Second)
+	// l1Setup.SimBackend.Commit()
+	// l2Setup.SimBackend.Commit()
+	// time.Sleep(1 * time.Second)
 
 	endBlockNumber, err := l2Setup.SimBackend.Client().BlockNumber(ctx)
 	require.NoError(t, err)
@@ -208,7 +206,7 @@ func testGERSyncer(t *testing.T, ctx context.Context,
 	l1Setup *helpers.L1Environment, l2Setup *helpers.L2Environment,
 	syncer *l2gersync.L2GERSync, i int) {
 	t.Helper()
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 
 	expectedGER, err := l1Setup.GERContract.GetLastGlobalExitRoot(&bind.CallOpts{Pending: false})
 	require.NoError(t, err)
