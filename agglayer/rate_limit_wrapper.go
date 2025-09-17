138ba5dcf61cfcb643638254dbad42d571eed4c7
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2025-09-17 11:39:13 +0200

    fix: conflicts and adapt rate_limit_wrapper

--- agglayer/rate_limit_wrapper.go
+++ agglayer/rate_limit_wrapper.go
@@ -115,6 +115,11 @@ func (r *RateLimitWrapper) GetLatestPendingCertificateHeader(
 	return r.client.GetLatestPendingCertificateHeader(ctx, networkID)
 }
 
+func (r *RateLimitWrapper) GetNetworkInfo(ctx context.Context, networkID uint32) (types.NetworkInfo, error) {
+	r.applyRateLimit("GetNetworkInfo")
+	return r.client.GetNetworkInfo(ctx, networkID)
+}
+
 // String returns a string representation of the rate limit wrapper
 func (r *RateLimitWrapper) String() string {
 	r.mu.RLock()
