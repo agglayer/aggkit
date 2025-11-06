d7d87dccf6cf54d4acc3ff10378f596cbd8fc818
Stefan Negovanović <stefan@ethernal.tech>
2025-11-06 13:02:26 +0100

    add ValidateGER function to aggsender validator proto def

--- aggsender/mocks/mock_aggsender_validator_client.go
+++ aggsender/mocks/mock_aggsender_validator_client.go
@@ -174,6 +174,80 @@ func (_c *AggsenderValidatorClient_ValidateCertificate_Call) RunAndReturn(run fu
 	return _c
 }
 
+// ValidateGER provides a mock function with given fields: ctx, in, opts
+func (_m *AggsenderValidatorClient) ValidateGER(ctx context.Context, in *v1.ValidateGERRequest, opts ...grpc.CallOption) (*v1.ValidateGERResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for ValidateGER")
+	}
+
+	var r0 *v1.ValidateGERResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *v1.ValidateGERRequest, ...grpc.CallOption) (*v1.ValidateGERResponse, error)); ok {
+		return rf(ctx, in, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *v1.ValidateGERRequest, ...grpc.CallOption) *v1.ValidateGERResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*v1.ValidateGERResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *v1.ValidateGERRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// AggsenderValidatorClient_ValidateGER_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ValidateGER'
+type AggsenderValidatorClient_ValidateGER_Call struct {
+	*mock.Call
+}
+
+// ValidateGER is a helper method to define mock.On call
+//   - ctx context.Context
+//   - in *v1.ValidateGERRequest
+//   - opts ...grpc.CallOption
+func (_e *AggsenderValidatorClient_Expecter) ValidateGER(ctx interface{}, in interface{}, opts ...interface{}) *AggsenderValidatorClient_ValidateGER_Call {
+	return &AggsenderValidatorClient_ValidateGER_Call{Call: _e.mock.On("ValidateGER",
+		append([]interface{}{ctx, in}, opts...)...)}
+}
+
+func (_c *AggsenderValidatorClient_ValidateGER_Call) Run(run func(ctx context.Context, in *v1.ValidateGERRequest, opts ...grpc.CallOption)) *AggsenderValidatorClient_ValidateGER_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		variadicArgs := make([]grpc.CallOption, len(args)-2)
+		for i, a := range args[2:] {
+			if a != nil {
+				variadicArgs[i] = a.(grpc.CallOption)
+			}
+		}
+		run(args[0].(context.Context), args[1].(*v1.ValidateGERRequest), variadicArgs...)
+	})
+	return _c
+}
+
+func (_c *AggsenderValidatorClient_ValidateGER_Call) Return(_a0 *v1.ValidateGERResponse, _a1 error) *AggsenderValidatorClient_ValidateGER_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *AggsenderValidatorClient_ValidateGER_Call) RunAndReturn(run func(context.Context, *v1.ValidateGERRequest, ...grpc.CallOption) (*v1.ValidateGERResponse, error)) *AggsenderValidatorClient_ValidateGER_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewAggsenderValidatorClient creates a new instance of AggsenderValidatorClient. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewAggsenderValidatorClient(t interface {
