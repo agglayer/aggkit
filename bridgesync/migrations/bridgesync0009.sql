b97e23b5d4eedda7477d449eff190a8f8c68ab03
Arpit Temani <temaniarpit27@gmail.com>
2025-11-10 13:56:56 +0400

    add index

--- bridgesync/migrations/bridgesync0009.sql
+++ bridgesync/migrations/bridgesync0009.sql
@@ -1,5 +1,7 @@
 -- +migrate Down
 DROP INDEX IF EXISTS idx_bridge_block_num_block_pos_asc;
+DROP INDEX IF EXISTS idx_claim_block_num_block_pos_asc;
 
 -- +migrate Up
 CREATE INDEX idx_bridge_block_num_block_pos_asc ON bridge (block_num ASC, block_pos ASC);
+CREATE INDEX idx_claim_block_num_block_pos_asc ON claim (block_num ASC, block_pos ASC);
