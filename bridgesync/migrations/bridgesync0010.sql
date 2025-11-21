77f0d3da5367d363ca892694110133d48a30717d
Stefan Negovanović <stefan@ethernal.tech>
2025-11-21 18:08:58 +0100

    create invalid_claim table

--- bridgesync/migrations/bridgesync0010.sql
+++ bridgesync/migrations/bridgesync0010.sql
@@ -1,6 +1,7 @@
 -- +migrate Down
 DROP TABLE IF EXISTS set_claim;
 DROP TABLE IF EXISTS unset_claim;
+DROP TABLE IF EXISTS invalid_claim;
 
 ALTER TABLE claim ADD COLUMN from_address VARCHAR;
 
@@ -22,6 +23,28 @@ CREATE TABLE set_claim (
 	global_index            TEXT NOT NULL,
 	created_at              INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
 	PRIMARY KEY (block_num, block_pos)
+);
+
+CREATE TABLE invalid_claim (
+    id                      INTEGER PRIMARY KEY AUTOINCREMENT,
+    block_num               INTEGER NOT NULL,
+    block_pos               INTEGER NOT NULL,
+    global_index            TEXT NOT NULL,
+    origin_network          INTEGER NOT NULL,
+    origin_address          VARCHAR NOT NULL,
+    destination_address     VARCHAR NOT NULL,
+    amount                  TEXT NOT NULL,
+    proof_local_exit_root   VARCHAR,
+    proof_rollup_exit_root  VARCHAR,
+    mainnet_exit_root       VARCHAR,
+    rollup_exit_root        VARCHAR,
+    global_exit_root        VARCHAR,
+    destination_network     INTEGER NOT NULL,
+    metadata                BLOB,
+    is_message              BOOLEAN,
+    block_timestamp         INTEGER,
+    created_at              INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
+    reason                  VARCHAR NOT NULL
 );
 
 ALTER TABLE claim DROP COLUMN from_address;
--- bridgesync/processor.go
+++ bridgesync/processor.go
@@ -38,6 +38,9 @@ const (
 	// claimTableName is the name of the table that stores claim events
 	claimTableName = "claim"
 
+	// invalidClaimTableName is the name of the table that stores invalid claim events
+	invalidClaimTableName = "invalid_claim"
+
 	// tokenMappingTableName is the name of the table that stores token mapping events
 	tokenMappingTableName = "token_mapping"
 
@@ -50,6 +53,21 @@ const (
 	// setClaimTableName is the name of the table that stores set claim events
 	setClaimTableName = "set_claim"
 )
+
+type DeleteClaimReason int
+
+const (
+	InvalidGERClaimCorrect DeleteClaimReason = iota
+)
+
+func (d DeleteClaimReason) String() string {
+	switch d {
+	case InvalidGERClaimCorrect:
+		return "invalid_ger_claim_correct"
+	default:
+		return "unknown"
+	}
+}
 
 const (
 	// orderByBlockDesc is the default order by clause for block-based queries
@@ -258,6 +276,56 @@ func (c *Claim) decodePreEtrogCalldata(data []any) (bool, error) {
 	c.GlobalExitRoot = crypto.Keccak256Hash(c.MainnetExitRoot.Bytes(), c.RollupExitRoot.Bytes())
 
 	return true, nil
+}
+
+type InvalidClaim struct {
+	// claim struct fields
+	ID                  int64          `meddler:"id,pk"`
+	BlockNum            uint64         `meddler:"block_num"`
+	BlockPos            uint64         `meddler:"block_pos"`
+	TxHash              common.Hash    `meddler:"tx_hash,hash"`
+	GlobalIndex         *big.Int       `meddler:"global_index,bigint"`
+	OriginNetwork       uint32         `meddler:"origin_network"`
+	OriginAddress       common.Address `meddler:"origin_address"`
+	DestinationAddress  common.Address `meddler:"destination_address"`
+	Amount              *big.Int       `meddler:"amount,bigint"`
+	ProofLocalExitRoot  types.Proof    `meddler:"proof_local_exit_root,merkleproof"`
+	ProofRollupExitRoot types.Proof    `meddler:"proof_rollup_exit_root,merkleproof"`
+	MainnetExitRoot     common.Hash    `meddler:"mainnet_exit_root,hash"`
+	RollupExitRoot      common.Hash    `meddler:"rollup_exit_root,hash"`
+	GlobalExitRoot      common.Hash    `meddler:"global_exit_root,hash"`
+	DestinationNetwork  uint32         `meddler:"destination_network"`
+	Metadata            []byte         `meddler:"metadata"`
+	IsMessage           bool           `meddler:"is_message"`
+	BlockTimestamp      uint64         `meddler:"block_timestamp"`
+	// additional fields
+	Reason    string    `meddler:"reason"`
+	CreatedAt time.Time `meddler:"created_at"`
+}
+
+// NewInvalidClaim creates a new InvalidClaim from a Claim and a reason
+func NewInvalidClaim(c *Claim, reason string) *InvalidClaim {
+	return &InvalidClaim{
+		BlockNum:            c.BlockNum,
+		BlockPos:            c.BlockPos,
+		TxHash:              c.TxHash,
+		GlobalIndex:         c.GlobalIndex,
+		OriginNetwork:       c.OriginNetwork,
+		OriginAddress:       c.OriginAddress,
+		DestinationAddress:  c.DestinationAddress,
+		Amount:              c.Amount,
+		ProofLocalExitRoot:  c.ProofLocalExitRoot,
+		ProofRollupExitRoot: c.ProofRollupExitRoot,
+		MainnetExitRoot:     c.MainnetExitRoot,
+		RollupExitRoot:      c.RollupExitRoot,
+		GlobalExitRoot:      c.GlobalExitRoot,
+		DestinationNetwork:  c.DestinationNetwork,
+		Metadata:            c.Metadata,
+		IsMessage:           c.IsMessage,
+		BlockTimestamp:      c.BlockTimestamp,
+		Reason:              reason,
+		CreatedAt:           time.Now().UTC(),
+	}
 }
 
 // TokenMapping representation of a NewWrappedToken event, that is emitted by the bridge contract
@@ -926,7 +994,8 @@ func (p *processor) ProcessBlock(ctx context.Context, block sync.Block) error {
 		}
 
 		if event.Claim != nil {
-			_, unsetCount, err := p.GetUnsetClaimsPaged(ctx, 1, 1, event.Claim.GlobalIndex)
+			globalIndex := event.Claim.GlobalIndex
+			_, unsetCount, err := p.GetUnsetClaimsPaged(ctx, 1, 1, globalIndex)
 			if err != nil {
 				p.log.Errorf("failed to get unset claims for global index %s at block %d: %v",
 					event.Claim.GlobalIndex.String(), block.Num, err)
@@ -934,19 +1003,23 @@ func (p *processor) ProcessBlock(ctx context.Context, block sync.Block) error {
 			}
 
 			if unsetCount == 0 {
-				claims, err := p.GetClaimsByGlobalIndex(ctx, event.Claim.GlobalIndex)
+				existingClaims, err := p.GetClaimsByGlobalIndex(ctx, globalIndex)
 				if err != nil {
 					p.log.Errorf("failed to get claims for global index %d at block %d: %v",
 						event.Claim.GlobalIndex, block.Num, err)
 					return err
 				}
 
-				if len(claims) > 0 {
-					// TODO: figure out if we need to keep those claims for history
-					_, err = tx.Exec(`DELETE FROM claim WHERE global_index = ?`, event.Claim.GlobalIndex.String())
+				if len(existingClaims) > 0 {
+					_, err = tx.Exec(`DELETE FROM claim WHERE global_index = ?`, globalIndex.String())
 					if err != nil {
-						p.log.Errorf("failed to delete claims for global index %d: %v",
-							event.Claim.GlobalIndex, err)
+						p.log.Errorf("failed to delete claims for global index %d: %v", globalIndex, err)
+						return err
+					}
+
+					invalidClaim := NewInvalidClaim(event.Claim, InvalidGERClaimCorrect.String())
+					if err = meddler.Insert(tx, invalidClaimTableName, invalidClaim); err != nil {
+						p.log.Errorf("failed to insert invalid claim for global index %d: %v", globalIndex, err)
 						return err
 					}
 				}
