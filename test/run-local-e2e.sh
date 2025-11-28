671e8abe35632c7d2f695c2187a0b6800c23b3a7
Arpit Temani <temaniarpit27@gmail.com>
2025-11-28 10:23:54 +0400

    run opsuccint

--- test/run-local-e2e.sh
+++ test/run-local-e2e.sh
@@ -173,14 +173,14 @@ if [ "$E2E_REPO_PATH" != "-" ]; then
     log_info "Running BATS E2E tests..."
     case "$TEST_TYPE" in
     single-l2-network-op-succinct)
-        bats ./tests/aggkit/bridge-e2e.bats || exit 1
-        bats ./tests/aggkit/e2e-pp.bats || exit 1
-        bats ./tests/aggkit/bridge-sovereign-chain-e2e.bats || exit 1
-        bats ./tests/aggkit/bridge-e2e-nightly.bats || exit 1
-        bats ./tests/aggkit/internal-claims.bats || exit 1
-        bats ./tests/aggkit/claim-reetrancy.bats || exit 1
-        bats ./tests/aggkit/aggsender-committee-updates.bats || exit 1
-        bats ./tests/op/optimistic-mode.bats || exit 1
+        bats ./tests/aggkit/bridge-e2e.bats -f "ERC20 token deposit L2 -> L1" || exit 1
+        # bats ./tests/aggkit/e2e-pp.bats || exit 1
+        # bats ./tests/aggkit/bridge-sovereign-chain-e2e.bats || exit 1
+        # bats ./tests/aggkit/bridge-e2e-nightly.bats || exit 1
+        # bats ./tests/aggkit/internal-claims.bats || exit 1
+        # bats ./tests/aggkit/claim-reetrancy.bats || exit 1
+        # bats ./tests/aggkit/aggsender-committee-updates.bats || exit 1
+        # bats ./tests/op/optimistic-mode.bats || exit 1
         ;;
     single-l2-network-op-succinct-aggoracle-committee)
         bats ./tests/aggkit/bridge-e2e-aggoracle-committee.bats || exit 1
