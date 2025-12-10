cf0968f81c00666b762ad0f3327799a2b1b8204e
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2025-12-10 10:40:29 +0100

    fix: PR comments

--- test/helpers/reorg.go
+++ test/helpers/reorg.go
@@ -28,7 +28,6 @@ func Reorg(t *testing.T, client *simulated.Backend, reorgSizeInBlocks uint64) {
 	currentBlockNum, err := client.Client().BlockNumber(ctx)
 	require.NoError(t, err)
 
-	block, err := client.Client().HeaderByNumber(ctx, big.NewInt(int64(currentBlockNum-reorgSizeInBlocks)))
 	header, err := client.Client().HeaderByNumber(ctx, new(big.Int).SetUint64(currentBlockNum-reorgSizeInBlocks))
 	log.Debugf("reorging until block %d. Current block %d (before reorg)", header.Number.Uint64(), currentBlockNum)
 	require.NoError(t, err)
