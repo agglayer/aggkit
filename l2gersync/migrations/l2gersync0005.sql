e05ca1a74c5efef92eea71fa62cf0dc2e9512e61
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2026-02-04 15:44:51 +0100

    fix: migration usign previous block_pos if not NULL

--- l2gersync/migrations/l2gersync0005.sql
+++ l2gersync/migrations/l2gersync0005.sql
@@ -1,24 +1,25 @@
 
 -- +migrate Down
+
 CREATE TABLE IF NOT EXISTS imported_global_exit_root (
     block_num INTEGER PRIMARY KEY REFERENCES block (num) ON DELETE CASCADE,
+    block_pos INTEGER,
     global_exit_root VARCHAR NOT NULL,
     l1_info_tree_index INTEGER NOT NULL
 );
 
-INSERT INTO imported_global_exit_root (block_num, global_exit_root, l1_info_tree_index)
-SELECT v2.block_num, v2.global_exit_root, v2.l1_info_tree_index
-FROM imported_global_exit_root_v2 v2
-JOIN (
-    SELECT block_num, MIN(block_pos) AS min_pos
-    FROM imported_global_exit_root_v2
-    GROUP BY block_num
-) pick
-ON pick.block_num = v2.block_num AND pick.min_pos = v2.block_pos;
+INSERT INTO imported_global_exit_root (block_num, block_pos, global_exit_root, l1_info_tree_index)
+SELECT
+    block_num,
+    block_pos,
+    global_exit_root,
+    l1_info_tree_index
+FROM imported_global_exit_root_v2;
 
 DROP TABLE IF EXISTS imported_global_exit_root_v2;
 
 -- +migrate Up
+
 CREATE TABLE
 	IF NOT EXISTS imported_global_exit_root_v2 (
 		block_num INTEGER NOT NULL,
@@ -37,7 +38,7 @@ INSERT INTO imported_global_exit_root_v2 (
 )
 SELECT
     block_num,
-    0 AS block_pos,
+    COALESCE(block_pos, 0) AS block_pos,
     global_exit_root,
     l1_info_tree_index
 FROM imported_global_exit_root;
