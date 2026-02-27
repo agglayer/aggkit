3c3f62725068e4b39053da895aa62750404c4197
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2026-02-27 14:55:08 +0100

    fix: optimized to_address sql script

--- bridgesync/migrations/bridgesync0013.sql
+++ bridgesync/migrations/bridgesync0013.sql
@@ -9,5 +9,8 @@ DROP TABLE _bridge_to_address_backup;
 CREATE TABLE _bridge_to_address_backup AS SELECT block_num, block_pos, to_address FROM bridge;
 ALTER TABLE bridge DROP COLUMN to_address;
 ALTER TABLE bridge ADD COLUMN to_address VARCHAR DEFAULT '';
-UPDATE bridge SET to_address = COALESCE((SELECT to_address FROM _bridge_to_address_backup b WHERE b.block_num = bridge.block_num AND b.block_pos = bridge.block_pos), '');
+-- Only set to_address for rows != null
+UPDATE bridge
+SET to_address = (SELECT b.to_address FROM _bridge_to_address_backup b WHERE b.block_num = bridge.block_num AND b.block_pos = bridge.block_pos)
+WHERE EXISTS (SELECT 1 FROM _bridge_to_address_backup b WHERE b.block_num = bridge.block_num AND b.block_pos = bridge.block_pos AND b.to_address IS NOT NULL);
 DROP TABLE _bridge_to_address_backup;
