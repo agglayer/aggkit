d808457c8d034b4fbc0790261e259286ca3512f2
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2026-02-27 14:57:18 +0100

    fix: preserve bridge.to_address

--- bridgesync/migrations/bridgesync0013.sql
+++ bridgesync/migrations/bridgesync0013.sql
@@ -1,7 +1,16 @@
 -- +migrate Down
+CREATE TABLE _bridge_to_address_backup AS SELECT block_num, block_pos, to_address FROM bridge;
 ALTER TABLE bridge DROP COLUMN to_address;
 ALTER TABLE bridge ADD COLUMN to_address VARCHAR;
+UPDATE bridge SET to_address = (SELECT to_address FROM _bridge_to_address_backup b WHERE b.block_num = bridge.block_num AND b.block_pos = bridge.block_pos);
+DROP TABLE _bridge_to_address_backup;
 
 -- +migrate Up
+CREATE TABLE _bridge_to_address_backup AS SELECT block_num, block_pos, to_address FROM bridge;
 ALTER TABLE bridge DROP COLUMN to_address;
 ALTER TABLE bridge ADD COLUMN to_address VARCHAR DEFAULT '';
+-- Only set to_address for rows != null
+UPDATE bridge
+SET to_address = (SELECT b.to_address FROM _bridge_to_address_backup b WHERE b.block_num = bridge.block_num AND b.block_pos = bridge.block_pos)
+WHERE EXISTS (SELECT 1 FROM _bridge_to_address_backup b WHERE b.block_num = bridge.block_num AND b.block_pos = bridge.block_pos AND b.to_address IS NOT NULL);
+DROP TABLE _bridge_to_address_backup;
