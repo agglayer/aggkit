419e8a4f1d1985fad48f84ae1320121b392191a7
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2026-03-04 12:21:04 +0100

    fix: OriginAddress and DestinationAddress are stored as BLOB in DB not in string

--- bridgesync/backfill_tx_sender.go
+++ bridgesync/backfill_tx_sender.go
@@ -124,9 +124,9 @@ type RecordToBackfill struct {
 	BlockTimestamp     uint64         `meddler:"block_timestamp"`
 	LeafType           uint8          `meddler:"leaf_type"`
 	OriginNetwork      uint32         `meddler:"origin_network"`
-	OriginAddress      common.Address `meddler:"origin_address,address"`
+	OriginAddress      common.Address `meddler:"origin_address"`
 	DestinationNetwork uint32         `meddler:"destination_network"`
-	DestinationAddress common.Address `meddler:"destination_address,address"`
+	DestinationAddress common.Address `meddler:"destination_address"`
 	Amount             *big.Int       `meddler:"amount,bigint"`
 	Metadata           []byte         `meddler:"metadata"`
 	DepositCount       uint32         `meddler:"deposit_count"`
